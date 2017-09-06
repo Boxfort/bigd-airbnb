355ddadaca12421e3b9983ea10effb7c6ed70bc8
Boxfort <40208539@live.napier.ac.uk>
2017-09-06 14:46:17 +0100

    Map now zooms to correct location

--- bigdairbnb/map/scripts/plotter.py
+++ bigdairbnb/map/scripts/plotter.py
@@ -4,15 +4,19 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'gmplot'))
 import gmplot
 
-filename = 'tomslee_airbnb_aarhus_1481_2017-07-28.csv'
-filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', filename)
+#TODO: Pull city information from firebase
+#def plot_heatmap(city_name, weight_on):
+if __name__ == '__main__':
+    filename = 'tomslee_airbnb_aarhus_1481_2017-07-28.csv'
+    filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', filename)
 
-data = pd.read_csv(filepath, sep=',')
-lats = data['latitude']
-longs = data['longitude']
-weights = data['price']
+    data = pd.read_csv(filepath, sep=',')
+    lats = data['latitude']
+    longs = data['longitude']
+    weights = data['price']
 
-gmap = gmplot.GoogleMapPlotter(37.428, -122.145, 16)
-gmap.heatmap_weight(lats, longs, weights)
+    #gmap = gmplot.GoogleMapPlotter(lats[0], longs[0], 16)
+    gmap = gmplot.GoogleMapPlotter.from_geocode("aarhus")
+    gmap.heatmap_weight(lats, longs, weights)
 
-gmap.draw('test.html')
+    gmap.draw('test.html')
