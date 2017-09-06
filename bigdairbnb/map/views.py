1386c557a6f4eb8304775c59af2a48d0acc0bdd9
Carlos <charliedn@Gmail.com>
2017-09-07 00:17:35 +0100

    first commit

--- bigdairbnb/map/views.py
+++ bigdairbnb/map/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
-import urllib
+import urllib.request
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'scripts'))
@@ -12,14 +12,20 @@ import plotter
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Map index page</h1>")
+    template = loader.get_template('map/index.html')
+    context = {
+            'thing_1' : 'This is thing one',
+            'thing_2' : 'This is thing two',
+            }
+    return HttpResponse(template.render(context, request))
 
 def city(request, city_name):
     template = loader.get_template('map/map.html')
-    map_file = urllib.urlopen(os.path.join(os.path.dirname(os.path.realpath(__file__)), "test.html")).read()
-    
+    map_file = urllib.request.urlopen('file://' + os.path.join(os.path.dirname(os.path.realpath(__file__)), "test.html")).read()
+    print("hehehehe")
     context = {
         'city_name': city_name,
         'map_file': map_file,
+        'thing_3' : 'okay',
     }
     return HttpResponse(template.render(context, request))
